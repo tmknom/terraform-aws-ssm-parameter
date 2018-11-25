0027e61a4b3b8b5cc96cdfadd43c1f598e0ef660
tmknom <tmk.nom@gmail.com>
2018-11-25 13:23:25 +0900

    Implement complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,1 +1,17 @@
+module "ssm_parameter" {
+  source = "../../"
+  name   = "example"
+  value  = "string value"
 
+  type            = "String"
+  description     = "This is example."
+  key_id          = ""
+  overwrite       = true
+  allowed_pattern = ""
+  enabled         = true
+
+  tags = {
+    Environment = "prod"
+    Name        = "example"
+  }
+}
