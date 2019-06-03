c88e23501ab7a146448bc2c6ed9d487e66e9180a
Taehyun Kim <kgyoo8232@gmail.com>
2019-06-03 12:14:00 +0900

    terraform 0.12 upgrade

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,25 @@
 output "ssm_parameter_arn" {
-  value       = "${join("", aws_ssm_parameter.default.*.arn)}"
+  value       = join("", aws_ssm_parameter.default.*.arn)
   description = "The ARN of the parameter."
 }
 
 output "ssm_parameter_name" {
-  value       = "${join("", aws_ssm_parameter.default.*.name)}"
+  value       = join("", aws_ssm_parameter.default.*.name)
   description = "The name of the parameter."
 }
 
 output "ssm_parameter_description" {
-  value       = "${join("", aws_ssm_parameter.default.*.description)}"
+  value       = join("", aws_ssm_parameter.default.*.description)
   description = "The description of the parameter."
 }
 
 output "ssm_parameter_type" {
-  value       = "${join("", aws_ssm_parameter.default.*.type)}"
+  value       = join("", aws_ssm_parameter.default.*.type)
   description = "The type of the parameter."
 }
 
 output "ssm_parameter_value" {
-  value       = "${join("", aws_ssm_parameter.default.*.value)}"
+  value       = join("", aws_ssm_parameter.default.*.value)
   description = "The value of the parameter."
 }
+
--- variables.tf
+++ variables.tf
@@ -1,52 +1,53 @@
 variable "name" {
-  type        = "string"
+  type        = string
   description = "The name of the parameter."
 }
 
 variable "value" {
   default     = "CHANGE ME"
-  type        = "string"
+  type        = string
   description = "The value of the parameter."
 }
 
 variable "type" {
   default     = "SecureString"
-  type        = "string"
+  type        = string
   description = "The type of the parameter. Valid types are String, StringList and SecureString."
 }
 
 variable "description" {
   default     = "Managed by Terraform"
-  type        = "string"
+  type        = string
   description = "The description of the parameter."
 }
 
 variable "key_id" {
   default     = ""
-  type        = "string"
+  type        = string
   description = "The KMS key id or arn for encrypting a SecureString."
 }
 
 variable "overwrite" {
   default     = false
-  type        = "string"
+  type        = string
   description = "Overwrite an existing parameter."
 }
 
 variable "allowed_pattern" {
   default     = ""
-  type        = "string"
+  type        = string
   description = "A regular expression used to validate the parameter value."
 }
 
 variable "tags" {
   default     = {}
-  type        = "map"
+  type        = map(string)
   description = "A mapping of tags to assign to the object."
 }
 
 variable "enabled" {
   default     = true
-  type        = "string"
+  type        = string
   description = "Set to false to prevent the module from creating anything."
 }
+
